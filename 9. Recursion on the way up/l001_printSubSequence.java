41dcb54fe49381632b4419136e0093275a956194
kuldeep2670 <kuldeepsingh2670@gmail.com>
2021-12-30 16:04:17 +0530

    recur

---
+++ 9. Recursion on the way up/l001_printSubSequence.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class l001_printSubSequence {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String s = scn.next();
+        printSS(s, "");
+        scn.close();
+    }
+
+    public static void printSS(String str, String ans) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);    //a
+        String ros = str.substring(1);  //bc
+        printSS(ros, ans + ch);
+        printSS(ros, ans);
+        
+    }
+}
