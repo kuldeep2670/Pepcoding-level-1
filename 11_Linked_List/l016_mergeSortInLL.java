a14b90c151b38029e5d4d1cb40687175449b0931
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-01-21 21:19:33 +0530

    dsa

--- 11_Linked_List/l016_mergeSortInLL.java
+++ 11_Linked_List/l016_mergeSortInLL.java
@@ -197,30 +197,39 @@ public class l016_mergeSortInLL {
             head = tail;
             tail = temp;
         }
-/**********************************Get Kth element from last*******************************/
+
+        /**********************************
+         * Get Kth element from last
+         *******************************/
         public int kthFromLast(int k) {
             Node slow = head;
             Node fast = head;
-            for(int i =0; i < k; i++){
+            for (int i = 0; i < k; i++) {
                 fast = fast.next;
             }
-            while(fast.next != null){
+            while (fast.next != null) {
                 slow = slow.next;
                 fast = fast.next;
             }
             return slow.data;
         }
-/************************************Mid element of Linked List***************************/
+
+        /************************************
+         * Mid element of Linked List
+         ***************************/
         public int mid() {
             Node slow = head;
             Node fast = head;
-            while(fast.next != null && fast.next.next != null){
+            while (fast.next != null && fast.next.next != null) {
                 slow = slow.next;
                 fast = fast.next.next;
             }
             return slow.data;
         }
-//*****************************************Merge two sorted lists**********************************/
+
+        /*****************************************
+         * Merge two sorted lists
+         **********************************/
         public static LinkedList mergeTwoSortedLists(LinkedList l1, LinkedList l2) {
             LinkedList l3 = new LinkedList();
             // c1 is current 1 and c2 is current 2
@@ -246,39 +255,47 @@ public class l016_mergeSortInLL {
             return l3;
         }
 
-//************************************Mid Node*****************************************/
-        public static Node midNode(Node head, Node tail){
+        /************************************
+         * Mid Node
+         *****************************************/
+        public static Node midNode(Node head, Node tail) {
             Node slow = head;
             Node fast = head;
 
-            while(fast != tail && fast.next != tail){
+            while (fast != tail && fast.next != tail) {
                 slow = slow.next;
                 fast = fast.next.next;
             }
             return slow;
         }
-/********************************Merge Sort***********************************************/
+
+        /********************************
+         * Merge Sort
+         ***********************************************/
         public static LinkedList mergeSort(Node head, Node tail) {
-            if(head == tail){
+            if (head == tail) {
                 LinkedList base = new LinkedList();
                 base.addFirst(head.data);
                 return base;
             }
-            
+
             Node mid = midNode(head, tail);
-            
-            LinkedList left = mergeSort(head, mid);   //left part
+
+            LinkedList left = mergeSort(head, mid); // left part
             LinkedList right = mergeSort(mid.next, tail); // right part
             return mergeTwoSortedLists(left, right);
         }
-//*********************************Remove Duplicates in Sorted Linked List*************************/
-        public void removeDuplicates(){
+
+        /*********************************
+         * Remove Duplicates in Sorted Linked List
+         *************************/
+        public void removeDuplicates() {
             LinkedList nl = new LinkedList();
-            while(this.size > 0){
+            while (this.size > 0) {
                 int val = this.getFirst();
                 this.removeFirst();
 
-                if(nl.size == 0 || nl.tail.data != val){
+                if (nl.size == 0 || nl.tail.data != val) {
                     nl.addLast(val);
                 }
             }
@@ -286,6 +303,200 @@ public class l016_mergeSortInLL {
             this.tail = nl.tail;
             this.size = nl.size;
         }
+
+        /**************************************
+         * Odd Even Linked List
+         **************************************/
+        public void oddEven() {
+            LinkedList odd = new LinkedList();
+            LinkedList even = new LinkedList();
+
+            while (this.size > 0) {
+                int val = this.getFirst();
+                this.removeFirst();
+
+                if (val % 2 == 0) {
+                    // even
+                    even.addLast(val);
+                } else {
+                    // odd
+                    odd.addLast(val);
+                }
+            }
+
+            if (odd.size > 0 && even.size > 0) { // both even and odd are present in list
+                odd.tail.next = even.head;
+                this.head = odd.head;
+                this.tail = even.tail;
+                this.size = odd.size + even.size;
+            } else if (odd.size > 0) { // only odd is present in list
+                this.head = odd.head;
+                this.tail = odd.tail;
+                this.size = odd.size;
+            } else if (even.size > 0) { // only even is present in list
+                this.head = even.head;
+                this.tail = even.tail;
+                this.size = even.size;
+            }
+        }
+
+        /***********************************
+         * k reverse linked list
+         ************************/
+        /*
+         * all groups of k elements in the list get reversed and linked.
+         * If the last set has less than k elements, leave it as it is (don't reverse)
+         */
+
+        public void kReverse(int k) {
+            if (k > this.size) {
+                return;
+            }
+            LinkedList ans = new LinkedList();
+            while (this.size >= k) {
+                LinkedList tmp = new LinkedList();
+                for (int i = 0; i < k; i++) {
+                    int val = this.getFirst();
+                    this.removeFirst();
+                    tmp.addFirst(val);
+                }
+
+                if (ans.size == 0) {
+                    ans = tmp;
+                } else {
+                    ans.tail.next = tmp.head;
+                    ans.tail = tmp.tail;
+                    ans.size += tmp.size;
+                }
+
+            }
+
+            // left elements
+            if (this.size > 0) {
+                ans.tail.next = this.head;
+                ans.tail = this.tail;
+                ans.size += this.size;
+            }
+
+            this.head = ans.head;
+            this.tail = ans.tail;
+            this.size = ans.size;
+        }
+
+        /**********************
+         * display reverse linked list (recursively)
+         *******************/
+        private void displayReverseHelper(Node node) {
+            if (node == null) {
+                return;
+            }
+            displayReverseHelper(node.next);
+            System.out.print(node.data + " ");
+        }
+
+        public void displayReverse() {
+            displayReverseHelper(head);
+            System.out.println();
+        }
+
+        /************************ reverse a linked list(pointer recursive) ***********/
+        private void reversePRHelper(Node node) {
+            if (node == null) {
+                return;
+            }
+            reversePRHelper(node.next);
+            if (node != tail) {
+                node.next.next = node;
+            }
+        }
+
+        public void reversePR() {
+            reversePRHelper(this.head);
+            head.next = null;
+            Node temp = head;
+            head = tail;
+            tail = temp;
+        }
+
+        /*********************** is linkedlist palindrome *********************/
+        Node pleft;
+
+        private boolean palindromeHelper(Node right) {
+            if (right == null) {
+                return true;
+            }
+
+            boolean recAns = palindromeHelper(right.next);
+            if (recAns == false) {
+                return false;
+            }
+            if (pleft.data != right.data) {
+                return false;
+            } else {
+                // same
+                pleft = pleft.next;
+                return true;
+            }
+        }
+
+        public boolean IsPalindrome() {
+            pleft = this.head;
+            return palindromeHelper(head);
+        }
+
+        /********************************* fold a linked list **********************/
+        /***************
+         * 1->2->3->4->5->6
+         * 1->6->2->5->3->4
+         **************************/
+        Node fleft;
+        private void foldHelper(Node right, int count) {
+            if (right == null) {
+                return;
+            }
+
+            foldHelper(right.next, count + 1);
+            if (count > size / 2) {
+                Node temp = fleft.next;
+                fleft.next = right;
+                right.next = temp;
+                fleft = temp;
+            } else if (count == size / 2) {
+                tail = right;
+                tail.next = null;
+            }
+
+        }
+
+        public void fold() {
+            fleft = head;
+            foldHelper(head, 0);
+        }
+
+        /***************intersection of two linked list */
+        public static int findIntersection(LinkedList one, LinkedList two){
+            Node c1 = one.head;
+            Node c2 = two.head;
+            
+            int d = Math.abs(one.size-two.size);
+            if(one.size > two.size ){
+                for(int i = 0; i<d; i++)
+                  c1 = c1.next;
+            }
+            else{
+                for(int i = 0; i<d; i++){
+                    c2 = c2.next;
+                }
+            }
+            
+            while(c1 != c2){
+                c1 = c1.next;
+                c2 = c2.next;
+            }
+            return c2.data; // c1.data;
+            
+          }
+
     }
 
     public static void main(String[] args) throws Exception {
