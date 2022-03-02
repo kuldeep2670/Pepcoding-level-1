437f3ee76253e49a63544a4820924285390aac52
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-03-02 22:27:41 +0530

    hashmap

---
+++ 13_Hashmap_andHeap/l001_hashmapDemo.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class l001_hashmapDemo {
+    public static void main(String args[]) {
+        //HashMap<key, value> 
+        HashMap<String, Integer> hm = new HashMap<>(); // initialise and declaration
+
+        //put
+        hm.put("India", 628);
+        hm.put("China", 837);
+        hm.put("Dubai", 120);
+
+        //  System.out.println(hm);
+
+        hm.put("India", 200);
+        hm.put("Pak", 837);
+        hm.put("US", 443);
+
+        // System.out.println(hm);
+        
+        // get
+        // int indData= hm.get("India");
+        //System.out.println(indData);
+
+        boolean isChinaPresent = hm.containsKey("China");
+        boolean isBangladeshPresent = hm.containsKey("Bangladesh");
+
+        System.out.println(isChinaPresent);
+        System.out.println(isBangladeshPresent);
+
+        //keyset
+        //Set<String> keys = hm.keySet();
+        for(String key : hm.keySet()){
+            System.out.println(key);
+        }
+
+        //size
+        System.out.println(hm.size());
+
+
+    }
+}
