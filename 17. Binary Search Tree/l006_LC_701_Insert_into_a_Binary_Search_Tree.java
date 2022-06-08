c6ca219607ced2bb162e8b86ec0bb9ce1b24fca5
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-06-08 19:42:21 +0530

    insert in bst

---
+++ 17. Binary Search Tree/l006_LC_701_Insert_into_a_Binary_Search_Tree.java
@@ -0,0 +1,32 @@
+public class l006_LC_701_Insert_into_a_Binary_Search_Tree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        } else if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+
+}
