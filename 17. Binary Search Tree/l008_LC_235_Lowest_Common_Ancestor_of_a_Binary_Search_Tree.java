c92f9a503aecd24e2b52b0086022df760717a706
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-06-21 13:34:28 +0530

    lca of bst

---
+++ 17. Binary Search Tree/l008_LC_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,25 @@
+public class l008_LC_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (p.val < curr.val && q.val < curr.val) {
+                curr = curr.left;
+            } else if (p.val > curr.val && q.val > curr.val) {
+                curr = curr.right;
+            } else {
+                return curr;
+            }
+        }
+        return null;
+    }
+}
