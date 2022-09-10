ca2b97100a060c0c0508bc2b5274fde0f87fd809
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-09-10 21:57:50 +0530

    method overloading

---
+++ 00. JAVA/l003_Method_Overloading.java
@@ -0,0 +1,14 @@
+// METHOD OVERLOADING
+//*If a class had multiple methods having same name but different in parameters it is known as method overloading.
+
+//Advantages of method overloading
+// * Method overloading increases the readability of the program
+// * Different ways to overload the method.
+// * Improves the code reusability
+
+// There are two ways to overload the method in java
+ // * By changing number of arguments and  data type.
+
+public class l003_Method_Overloading {
+    
+}
